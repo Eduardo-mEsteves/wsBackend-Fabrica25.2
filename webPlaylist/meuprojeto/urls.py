8d1005cf22dea444197622be94bd8805afbde57a
Eduardo-mEsteves <eduardofrancamesteves@gmail.com>
2025-09-13 15:32:02 -0300

    ✨ feat: Segundo app criado
    App para a manipulação da segunda entidade criado.

--- webPlaylist/meuprojeto/urls.py
+++ webPlaylist/meuprojeto/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('meuapp.urls'))
+    path('', include('meuapp.urls')),
+    path('', include('meusartistas.urls'))
 ]
---
+++ webPlaylist/meusartistas/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MeusartistasConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'meusartistas'
